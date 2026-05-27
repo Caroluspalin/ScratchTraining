6d2475b29eb2d5afae89ee9bfe0f0d8e2f49fe82
Carolus <caroluspalin@icloud.com>
2026-05-27 13:41:55 +0300

    Java training

--- Javatasks/calculator.java
+++ Javatasks/calculator.java
@@ -1,24 +1,53 @@
 import java.util.Scanner;
 
-public class switchCaseTest {
+public class calculator {
     public static void main(String[] args) {
 
-        String paeva;
-        
         Scanner scanner = new Scanner(System.in);
 
-        System.out.print("Mikä päivä tänään on: ");
-        paeva = scanner.nextLine();
+        double num1;
+        double num2;
+        double result = 0;
+        char operator;
+        Boolean isValid = true;
 
+        System.out.print("Enter first number: ");
+        num1 = scanner.nextDouble();
+
+        System.out.print("Enter an operator (+,-,*,/,^): ");
+        operator = scanner.next().charAt(0);
+
+        System.out.print("Enter the second number: ");
+        num2 = scanner.nextDouble();
         
-        
-        switch(paeva) {
-            case "Maanantai", "Tiistai", "Keskiviikko", "Torstai" -> System.out.println("On vielä arkiviikko, tsemppiä");
-            case "Perjantai" -> System.out.println("Tänään alkaa,nyt vielä töihin");
-            case "Lauantai", "Sunnuntai" -> System.out.println("Viikonloppu käynnissä");
-            default -> System.out.println("Syöttämäsi päivä ei ole validi");
+        switch(operator) {
+            case '+' -> result = num1 + num2;
+            case '-' -> result = num1 - num2;
+            case '*' -> result = num1 * num2;
+            case '/' -> {
+                if (num2 == 0) {
+                    System.out.println("Cannot divide by zero");
+                    isValid = false;
+                }
+                else {
+                    result = num1 / num2;
+                }
+            }
+            case '^' -> result = Math.pow(num1, num2);
+            default -> {
+                System.out.println("Invalid operator!");
+                isValid = false;
+            }  
+    }
+        if (isValid) {
+            System.out.printf("%.2f\n", result);
         }
 
+
+        
+
         scanner.close();
+
+
     }
 }
--- Javatasks/switchCaseTest.java
+++ Javatasks/switchCaseTest.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 
-public class calculator {
+public class switchCaseTest {
     public static void main(String[] args) {
 
         String paeva;
